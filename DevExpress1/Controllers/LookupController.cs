bc94a110606e677eafab5132264703d21f8c1720
Yusuf Boyacı <85557228+yusufboyaci@users.noreply.github.com>
2022-04-05 11:32:00 +0300

    ----

---
+++ DevExpress1/Controllers/LookupController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevExpress1.Controllers
+{
+    public class LookupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
