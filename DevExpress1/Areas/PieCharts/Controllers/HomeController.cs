07a2565c347de44af74924ec2aa7d52799cd13a8
Yusuf Boyacı <85557228+yusufboyaci@users.noreply.github.com>
2022-04-06 17:26:21 +0300

    ----

---
+++ DevExpress1/Areas/PieCharts/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevExpress1.Areas.PieCharts.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("PieCharts")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DevExpress1/Areas/PieCharts/Controllers/PieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevExpress1.Areas.PieCharts.Controllers
+{
+    public class PieController : Controller
+    {
+        [Area("PieCharts")]
+        public IActionResult Pie()
+        {
+            return View();
+        }
+    }
+}
