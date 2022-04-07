5c659ad84f32cc8270d819a452890cdb7da290c3
Yusuf Boyacı <85557228+yusufboyaci@users.noreply.github.com>
2022-04-07 12:15:44 +0300

    ----

---
+++ DevExpress1/Areas/BarCharts/Controllers/BarCharController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevExpress1.Areas.BarCharts.Controllers
+{
+    [Area("BarCharts")]
+    public class BarCharController : Controller
+    {
+        public IActionResult BarChart()
+        {
+            return View();
+        }
+    }
+}
