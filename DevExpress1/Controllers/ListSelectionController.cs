467866587c7309db807b23c64bdb52b8a384eb7a
Yusuf Boyacı <85557228+yusufboyaci@users.noreply.github.com>
2022-06-22 16:18:20 +0300

    ----

---
+++ DevExpress1/Controllers/ListSelectionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevExpress1.Controllers
+{
+    public class ListSelectionController : Controller
+    {
+        public IActionResult ListSelection()
+        {
+            return View();
+        }
+    }
+}
--- DevExpress1/Startup.cs
+++ DevExpress1/Startup.cs
@@ -26,7 +26,7 @@ namespace DevExpress1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDevExpressControls();
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
